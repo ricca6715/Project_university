6a16459506480e102a41939ebcc9cf49c180e0f7
ricca6715 <riccardocontino94@gmail.com>
2018-08-06 11:41:30 +0200

    added test classes

---
+++ src/test/java/it/unisalento/se/saw/restapi/MaterialSatisfactionRestControllerTest.java
@@ -0,0 +1,87 @@
+package it.unisalento.se.saw.restapi;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.support.ServletContextLiveBeansView;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import it.unisalento.se.saw.Iservices.ILectureSatisfactionService;
+import it.unisalento.se.saw.Iservices.IMaterialSatisfactionService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MaterialSatisfactionRestControllerTest {
+	
+	
+	private MockMvc mockMvc;
+	
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+			MediaType.APPLICATION_JSON.getType(), 
+			MediaType.APPLICATION_JSON.getSubtype(),
+			Charset.forName("utf8")
+			);
+	@Mock
+	private IMaterialSatisfactionService materialSatisfactionServiceMock;
+	
+	@Before
+	public void setUp() {
+		
+		mockMvc = MockMvcBuilders.standaloneSetup(new MaterialSatisfactionRestController(materialSatisfactionServiceMock))
+					.setViewResolvers(viewResolver())
+					.build();
+	}
+	
+	
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/templates/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+
+}
